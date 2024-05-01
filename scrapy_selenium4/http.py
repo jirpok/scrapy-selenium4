4237259072697f956d043daa73b5b9c6d08f081f
Jiri <346563+007C39@users.noreply.github.com>
2024-05-01 10:50:16 +0200

    Add type hints

--- scrapy_selenium4/http.py
+++ scrapy_selenium4/http.py
@@ -1,27 +1,35 @@
-"""This module contains the ``SeleniumRequest`` class"""
+from scrapy.http import Request
 
-from scrapy import Request
+from selenium.webdriver.support import expected_conditions
 
 
 class SeleniumRequest(Request):
-    """Scrapy ``Request`` subclass providing additional arguments"""
+    """Subclass of `scrapy.http.Request` providing additional arguments."""
 
-    def __init__(self, wait_time=None, wait_until=None, screenshot=False, script=None, *args, **kwargs):
-        """Initialize a new selenium request
+    def __init__(
+        self,
+        wait_time: int | None = None,
+        wait_until=None,
+        screenshot: bool = False,
+        script: str | None = None,
+        *args,
+        **kwargs
+    ):
+        """Initialize new Selenium request.
 
         Parameters
         ----------
-        wait_time: int
-            The number of seconds to wait.
-        wait_until: method
-            One of the "selenium.webdriver.support.expected_conditions". The response
-            will be returned until the given condition is fulfilled.
-        screenshot: bool
-            If True, a screenshot of the page will be taken and the data of the screenshot
-            will be returned in the response "meta" attribute.
-        script: str
-            JavaScript code to execute.
-
+        wait_time : int | None, optional
+            The number of seconds to wait, by default `None`.
+        wait_until : _type_, optional
+            One of the `selenium.webdriver.support.expected_conditions`.
+            The response will not be returned until the given condition
+            is fulfilled. By default `None`.
+        screenshot : bool, optional
+            If `True`, takes a screenshot of the page and returns the
+            data in the response `meta` attribute. By default `False`.
+        script : str | None, optional
+            JavaScript code to execute. By default `None`.
         """
 
         self.wait_time = wait_time
