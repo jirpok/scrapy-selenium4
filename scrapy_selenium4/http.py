7817265b614b61667fdc4eed36663762a4006df8
Jiri <346563+007C39@users.noreply.github.com>
2024-05-20 11:53:35 +0200

    Update docstrings

--- scrapy_selenium4/http.py
+++ scrapy_selenium4/http.py
@@ -1,37 +1,36 @@
-from scrapy.http import Request
+from typing import Any
 
-from selenium.webdriver.support import expected_conditions
+from scrapy.http import Request
 
 
 class SeleniumRequest(Request):
     """Subclass of `scrapy.http.Request` providing additional arguments."""
 
-    def __init__(
-        self,
-        wait_time: int | None = None,
-        wait_until=None,
-        screenshot: bool = False,
-        script: str | None = None,
-        *args,
-        **kwargs
-    ) -> None:
-        """Initialize new Selenium request.
-
+    def __init__(self,
+                 wait_time: int | None = None,
+                 wait_until: Any | None = None,
+                 screenshot: bool = False,
+                 script: str | None = None,
+                 *args,
+                 **kwargs) -> None:
+        """
         Parameters
         ----------
         wait_time : int | None, optional
-            The number of seconds to wait, by default `None`.
-        wait_until : _type_, optional
-            One of the `selenium.webdriver.support.expected_conditions`.
+            The number of seconds to wait. By default `None`.
+
+        wait_until : Any | None, optional
+             One of the `selenium.webdriver.support.expected_conditions`.
             The response will not be returned until the given condition
             is fulfilled. By default `None`.
+
         screenshot : bool, optional
-            If `True`, takes a screenshot of the page and returns the
+            If `True`, take a screenshot of the page and return the
             data in the response `meta` attribute. By default `False`.
+
         script : str | None, optional
             JavaScript code to execute. By default `None`.
         """
-
         self.wait_time = wait_time
         self.wait_until = wait_until
         self.screenshot = screenshot
